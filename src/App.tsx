338c9c36ec71af51e67eaabbca4f70ba18e7fa40
Mark Edosa <maqui7295@gmail.com>
2020-06-15 18:18:58 +0100

    basic UI setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import './App.css';
 import Main from './components/Main';
+import useImportScript from './ImportScript';
+
 
 function App() {
+
+  useImportScript("/path/to/resource.js");
+
   return (
     <Main />
   );
--- src/components/Hospitals.tsx
+++ src/components/Hospitals.tsx
@@ -25,6 +25,48 @@ const data = [
 ];
 
 export default function Hospitals(props:object) {
+
+
+  function getPlaces() {
+    // Create the places service.
+    // ts-nocheck
+    var service = new google.maps.places.PlacesService(map);
+
+    // Perform a nearby search.
+    service.nearbySearch({
+      location: pyrmont,
+      radius: 4000,
+      type: ['hospital']
+    },
+      function (results, status, pagination) {
+        if (status !== 'OK') return;
+
+        displayResults(results);
+        getNextPage = pagination.hasNextPage && function () {
+          pagination.nextPage();
+        };
+      });
+  }
+
+  function getPosition(): object {
+    var pyrmont = {
+      lat: 23.8701334,
+      lng: 90.2713944
+    };
+    if (navigator.geolocation) {
+      try {
+        navigator.geolocation.getCurrentPosition(function (position) {
+          var pyrmont = {
+            lat: position.coords.latitude,
+            lng: position.coords.longitude
+          };
+        });
+      } catch (err) {
+        displayError(err);
+      }
+      return pyrmont;
+    }
+
   
   return (
     <>
